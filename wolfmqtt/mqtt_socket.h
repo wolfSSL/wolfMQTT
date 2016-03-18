449bf467217fd5d57eac068b0db593eeb42447da
David Garske <david@wolfssl.com>
2016-03-18 10:24:43 -0700

    Updated Copyright for all .h and .c files for v0.6 release.

--- wolfmqtt/mqtt_socket.h
+++ wolfmqtt/mqtt_socket.h
@@ -1,6 +1,6 @@
 /* mqtt_socket.h
  *
- * Copyright (C) 2006-2015 wolfSSL Inc.
+ * Copyright (C) 2006-2016 wolfSSL Inc.
  *
  * This file is part of wolfMQTT.
  *
@@ -16,7 +16,7 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1335, USA
  */
 
 /* Implementation by: David Garske
