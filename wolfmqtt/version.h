4fc04011b095be85ce7e623d71670f31ae0bd42f
David Garske <david@wolfssl.com>
2016-03-18 10:09:13 -0700

    Updates for wolfMQTT v0.6 release.

--- wolfmqtt/version.h
+++ wolfmqtt/version.h
@@ -34,8 +34,8 @@
 extern "C" {
 #endif
 
-#define LIBWOLFMQTT_VERSION_STRING "0.5"
-#define LIBWOLFMQTT_VERSION_HEX 0x00005000
+#define LIBWOLFMQTT_VERSION_STRING "0.6"
+#define LIBWOLFMQTT_VERSION_HEX 0x00006000
 
 #ifdef __cplusplus
 }
