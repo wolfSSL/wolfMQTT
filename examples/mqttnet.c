30f9356fe0d991f553251779e4d1a5ce92815cbd
David Garske <david@wolfssl.com>
2017-04-05 06:56:05 -0700

    Fix build error with non-blocking.

--- examples/mqttnet.c
+++ examples/mqttnet.c
@@ -369,7 +369,7 @@ static int NetRead(void *context, byte* buf, int buf_len,
     SocketContext *sock = (SocketContext*)context;
     int rc = -1, timeout = 0;
     SOERROR_T so_error = 0;
-#ifndef WOLFMQTT_NO_TIMEOUT
+#if !defined(WOLFMQTT_NO_TIMEOUT) && !defined(WOLFMQTT_NONBLOCK)
     fd_set recvfds;
     fd_set errfds;
     struct timeval tv;
