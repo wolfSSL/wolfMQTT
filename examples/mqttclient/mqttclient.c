1796b506ee5b70b83aa63be5c3e4e56d3eef4812
David Garske <david@wolfssl.com>
2017-05-31 10:33:58 -0700

    Adds support for keep-alive ping when using non-blocking mode. Checks for timeout with no activity using command timeout (-C arg). New `mqtt_check_timeout` function in `mqttexample.c`. Support for Posix time and Microchip Harmony. Added printf message for keep-alive.

--- examples/mqttclient/mqttclient.c
+++ examples/mqttclient/mqttclient.c
@@ -278,6 +278,12 @@ int mqttclient_test(MQTTCtx *mqttCtx)
                     break;
                 }
 
+            #ifdef WOLFMQTT_NONBLOCK
+                /* Track elapsed time with no activity and trigger timeout */
+                rc = mqtt_check_timeout(rc, &mqttCtx->start_sec,
+                    mqttCtx->cmd_timeout_ms/1000);
+            #endif
+
                 /* check return code */
                 if (rc == MQTT_CODE_CONTINUE) {
                     return rc;
@@ -307,6 +313,8 @@ int mqttclient_test(MQTTCtx *mqttCtx)
             #endif
                 else if (rc == MQTT_CODE_ERROR_TIMEOUT) {
                     /* Keep Alive */
+                    PRINTF("Keep-alive timeout, sending ping");
+
                     rc = MqttClient_Ping(&mqttCtx->client);
                     if (rc == MQTT_CODE_CONTINUE) {
                         return rc;
