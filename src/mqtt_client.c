6d589938c1a8858fd93ab3e46af9091d83a94bc8
David Garske <david@wolfssl.com>
2020-04-20 10:12:57 -0700

    Fixed unused arg in new API.

--- src/mqtt_client.c
+++ src/mqtt_client.c
@@ -2049,6 +2049,8 @@ int MqttClient_GetProtocolVersion(MqttClient *client)
 #ifdef WOLFMQTT_V5
     if (client && client->protocol_level == MQTT_CONNECT_PROTOCOL_LEVEL_5)
         return MQTT_CONNECT_PROTOCOL_LEVEL_5;
+#else
+    (void)client;
 #endif
     return MQTT_CONNECT_PROTOCOL_LEVEL_4;
 }
