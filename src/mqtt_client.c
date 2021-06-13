c3f8cc621b981b5ffa5a176fb025e3c5a61485b7
Yonggang Luo <luoyonggang@gmail.com>
2021-06-14 02:56:40 +0800

    Turn MqttClient_Disconnect_ex to use MqttClient_SendObject
    Signed-off-by: Yonggang Luo <luoyonggang@gmail.com>

--- src/mqtt_client.c
+++ src/mqtt_client.c
@@ -1883,60 +1883,37 @@ int MqttClient_Ping(MqttClient *client)
 
 int MqttClient_Disconnect(MqttClient *client)
 {
-    return MqttClient_Disconnect_ex(client, NULL);
+    MqttDisconnect disconnect;
+    XMEMSET(&disconnect, 0, sizeof(disconnect));
+    for (;;) {
+        int rc = MqttClient_Disconnect_ex(client, &disconnect);
+    #ifdef WOLFMQTT_NONBLOCK
+        if (rc == MQTT_CODE_CONTINUE)
+            continue;
+    #endif
+        return rc;
+    }
 }
 
 int MqttClient_Disconnect_ex(MqttClient *client, MqttDisconnect *disconnect)
 {
-    int rc, len;
+    MqttSendObjectOption send_option;
 
     /* Validate required arguments */
-    if (client == NULL) {
+    if (client == NULL || disconnect == NULL) {
         return MQTT_CODE_ERROR_BAD_ARG;
     }
-
 #ifdef WOLFMQTT_V5
-    if (disconnect != NULL) {
-        /* Use specified protocol version if set */
-        disconnect->protocol_level = client->protocol_level;
-    }
+    /* Use specified protocol version if set */
+    disconnect->protocol_level = client->protocol_level;
 #endif
 
-#ifdef WOLFMQTT_MULTITHREAD
-    /* Lock send socket mutex */
-    rc = wm_SemLock(&client->lockSend);
-    if (rc != 0) {
-        return rc;
-    }
-#endif
-
-    /* Encode the disconnect packet */
-    rc = MqttEncode_Disconnect(client->tx_buf, client->tx_buf_len, disconnect);
-#ifdef WOLFMQTT_DEBUG_CLIENT
-    PRINTF("MqttClient_EncodePacket: Len %d, Type %s (%d), ID %d, QoS %d",
-        rc, MqttPacket_TypeDesc(MQTT_PACKET_TYPE_DISCONNECT),
-        MQTT_PACKET_TYPE_DISCONNECT, 0, 0);
-#endif
-    if (rc <= 0) {
-    #ifdef WOLFMQTT_MULTITHREAD
-        wm_SemUnlock(&client->lockSend);
-    #endif
-        return rc;
-    }
-    len = rc;
-
-    /* Send disconnect packet */
-    rc = MqttPacket_Write(client, client->tx_buf, len);
-#ifdef WOLFMQTT_MULTITHREAD
-    wm_SemUnlock(&client->lockSend);
-#endif
-    if (rc != len) {
-        return rc;
-    }
-
-    /* No response for MQTT disconnect packet */
-
-    return MQTT_CODE_SUCCESS;
+    XMEMSET(&send_option, 0, sizeof(send_option));
+    send_option.send_packet_type = MQTT_PACKET_TYPE_DISCONNECT;
+    send_option.ack_packet_type = MQTT_PACKET_TYPE_RESERVED;
+    send_option.send_obj = disconnect;
+    send_option.timeout_ms = client->cmd_timeout_ms;
+    return MqttClient_SendObjectWaitType(client, &send_option);
 }
 
 #ifdef WOLFMQTT_V5
