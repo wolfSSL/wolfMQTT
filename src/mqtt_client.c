6ca08051917de2ad5916bc4f7dfd7524310ba93c
Yonggang Luo <luoyonggang@gmail.com>
2021-06-15 22:37:24 +0800

    Refactoring MqttClient_Auth to use MqttClient_SendObjectWaitType

--- src/mqtt_client.c
+++ src/mqtt_client.c
@@ -2312,88 +2312,22 @@ int MqttClient_Disconnect_ex(MqttClient *client, MqttDisconnect *disconnect)
 #ifdef WOLFMQTT_V5
 int MqttClient_Auth(MqttClient *client, MqttAuth* auth)
 {
-    int rc, len;
+    MqttSendObjectOption send_option;
 
     /* Validate required arguments */
     if (client == NULL) {
         return MQTT_CODE_ERROR_BAD_ARG;
     }
-
-    if (auth->stat.write == MQTT_MSG_BEGIN) {
-    #ifdef WOLFMQTT_MULTITHREAD
-        /* Lock send socket mutex */
-        rc = wm_SemLock(&client->lockSend);
-        if (rc != 0) {
-            return rc;
-        }
-    #endif
-
-        /* Encode the authentication packet */
-        rc = MqttEncode_Auth(client->tx_buf, client->tx_buf_len, auth);
-    #ifdef WOLFMQTT_DEBUG_CLIENT
-        PRINTF("MqttClient_EncodePacket: Len %d, Type %s (%d), ID %d, QoS %d",
-            rc, MqttPacket_TypeDesc(MQTT_PACKET_TYPE_AUTH),
-            MQTT_PACKET_TYPE_AUTH, 0, 0);
-    #endif
-        if (rc <= 0) {
-        #ifdef WOLFMQTT_MULTITHREAD
-            wm_SemUnlock(&client->lockSend);
-        #endif
-            return rc;
-        }
-        len = rc;
-
-    #ifdef WOLFMQTT_MULTITHREAD
-        rc = wm_SemLock(&client->lockClient);
-        if (rc == 0) {
-            /* inform other threads of expected response */
-            rc = MqttClient_RespList_Add(client, MQTT_PACKET_TYPE_AUTH, 0,
-                &auth->pendResp, auth);
-            wm_SemUnlock(&client->lockClient);
-        }
-        if (rc != 0) {
-            wm_SemUnlock(&client->lockSend);
-            return rc; /* Error locking client */
-        }
-    #endif
-
-        /* Send authentication packet */
-        rc = MqttPacket_Write(client, client->tx_buf, len);
-    #ifdef WOLFMQTT_MULTITHREAD
-        wm_SemUnlock(&client->lockSend);
-    #endif
-        if (rc != len) {
-        #ifdef WOLFMQTT_MULTITHREAD
-            if (wm_SemLock(&client->lockClient) == 0) {
-                MqttClient_RespList_Remove(client, &auth->pendResp);
-                wm_SemUnlock(&client->lockClient);
-            }
-        #endif
-            return rc;
-        }
-
-        auth->stat.write = MQTT_MSG_WAIT;
-    }
-
-    /* Wait for auth packet */
-    rc = MqttClient_WaitType(client, auth, MQTT_PACKET_TYPE_AUTH, 0,
-        client->cmd_timeout_ms);
-#ifdef WOLFMQTT_NONBLOCK
-    if (rc == MQTT_CODE_CONTINUE)
-        return rc;
-#endif
-
+    XMEMSET(&send_option, 0, sizeof(send_option));
+    send_option.send_packet_type = MQTT_PACKET_TYPE_AUTH;
+    send_option.ack_packet_type = MQTT_PACKET_TYPE_AUTH;
+    send_option.send_obj = auth;
+    send_option.recv_obj = auth;
 #ifdef WOLFMQTT_MULTITHREAD
-    if (wm_SemLock(&client->lockClient) == 0) {
-        MqttClient_RespList_Remove(client, &auth->pendResp);
-        wm_SemUnlock(&client->lockClient);
-    }
+    send_option.pend_resp = &auth->pendResp;
 #endif
-
-    /* reset state */
-    auth->stat.write = MQTT_MSG_BEGIN;
-
-    return rc;
+    send_option.timeout_ms = client->cmd_timeout_ms;
+    return MqttClient_SendObjectWaitType(client, &send_option);
 }
 
 MqttProp* MqttClient_PropsAdd(MqttProp **head)
